e7a2c4ae794bbfe20b65413dcbe6f0c7c8f7d5aa
Lemon <congphuoc2004@gmail.com>
2026-03-11 08:41:14 +0700

    B Push lan 2

--- BaiTap/src/routes/web.js
+++ BaiTap/src/routes/web.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 const productController = require('../controllers/productController');
+const aboutController = require('../controllers/aboutController');
 
 // Khai báo route cho người A (Home)
 router.get('/', homeController.getHomePage);
 
 // Khai báo route cho người B (Product)
 router.get('/products', productController.getProductPage);
+
+// Khai báo route để test push code
+router.get('/about', aboutController.getAboutPage);
 
 module.exports = router;
