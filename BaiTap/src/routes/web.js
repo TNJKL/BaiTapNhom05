54d8678d007b12e8ca0b95554f8e294178781d52
TNJKL <sinhngay21042004@gmail.com>
2026-03-11 08:44:36 +0700

    A push lan 3

--- BaiTap/src/routes/web.js
+++ BaiTap/src/routes/web.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 const productController = require('../controllers/productController');
 const aboutController = require('../controllers/aboutController');
+const contactController = require('../controllers/contactController');
 
 // Khai báo route cho người A (Home)
 router.get('/', homeController.getHomePage);
@@ -12,5 +13,8 @@ router.get('/products', productController.getProductPage);
 
 // Khai báo route để test push code
 router.get('/about', aboutController.getAboutPage);
+
+// Khai báo route liên hệ (tính năng mới để thực hành git)
+router.get('/contact', contactController.getContactPage);
 
 module.exports = router;
